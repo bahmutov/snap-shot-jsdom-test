7462e724479d2604bbc35d670273b69ace6e5672
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-13 10:09:51 -0500

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# snap-shot-jsdom-test
+
+> Jest compatibility with [snap-shot][snap-shot] test project
+
+[![Build status][ci-image] ][ci-url]
+
+* DOM element serialized with
+  [package-snapshot-dom](https://github.com/wildpeaks/package-snapshot-dom)
+* output HTML can be pretty formatted with
+  [posthtml-beautify](https://github.com/gitscrum/posthtml-beautify)
+  or other processors
+
+[snap-shot]: https://github.com/bahmutov/snap-shot
+[ci-image]: https://travis-ci.org/bahmutov/snap-shot-jsdom-test.svg?branch=master
+[ci-url]: https://travis-ci.org/bahmutov/snap-shot-jsdom-test
--- spec.js
+++ spec.js
@@ -1,4 +1,15 @@
 const la = require('lazy-ass')
+const elementToJson = require('@wildpeaks/snapshot-dom').toJSON
+const clean = require('clean-html').clean
+const snapshot = require('snap-shot')
+
+const pretty = html => {
+  let result
+  clean(html, out => {
+    result = out
+  })
+  return result
+}
 
 describe('snap-shot DOM', function () {
   it('has document', function () {
@@ -8,5 +19,21 @@ describe('snap-shot DOM', function () {
   it('creates DIV', function () {
     const div = document.createElement('div')
     la(div.nodeName === 'DIV')
+  })
+
+  it('has element to JSON', function () {
+    la(typeof elementToJson === 'function')
+  })
+
+  it('can serialize div', function () {
+    const div = document.createElement('div')
+    div.innerHTML = '<p class="hello">Hello World</p>'
+    snapshot(elementToJson(div))
+  })
+
+  it('can serialize div using js-beautify.html', function () {
+    const div = document.createElement('div')
+    div.innerHTML = '<div class="hello"><div id="message">Hello World</div></div>'
+    snapshot(pretty(div.innerHTML))
   })
 })
